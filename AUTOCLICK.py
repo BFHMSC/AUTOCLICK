f10be47955b788a998a391fd6871364016d7acf8
BFHMSC <103470790+BFHMSC@users.noreply.github.com>
2022-04-12 09:57:01 +0700

    Add files via upload

---
+++ AUTOCLICK.py
@@ -0,0 +1,9 @@
+###lock script python by botfarhack
+import base64, codecs
+magic = 'aW1wb3J0IHB5YXV0b2d1aSwgdGltZSwgc3lzLCByYW5kb20NCg0KZGVmIGF1dG9fY2xpY2soZGVsYXkpOg0KICAgIHB5YXV0b2d1aS5jbGljaygpDQogICAgcHlhdXRvZ3VpLmNsaWNrKCkNCiAgICB0aW1lLnNsZWV'
+love = 'jXTEyoTS5XD0XVPNtVUO5LKI0o2q1nF5woTywnltcQDbtVPNtpUyuqKEiM3IcYzAfnJAeXPxAPvNtVPNAPzyzVS9sozSgMI9sVQ09VPqsK21unJ5sKlp6VPNtVN0XVPNtVUW1oz5cozptCFOHpaIyQDbtVPNtL291oa'
+god = 'RlciA9IDANCiAgICBtdWx0aXBsaWVyID0gcmFuZG9tLnJhbmRvbSgpDQogICAgaW50ZXJ2YWwgPSAwLjINCiAgICB3aGlsZSBydW5uaW5nOg0KICAgICAgICBjb3VudGVyICs9IDENCiAgICAgICAgaWYgY291bnRlc'
+destiny = 'vN+CFN1ZQbAPvNtVPNtVPNtVPNtVTAiqJ50MKVtCFNjVPNtQDbtVPNtVPNtVPNtVPOgqJk0nKOfnJIlVQ0tpzShMT9gYaWuozEioFtcQDbtVPNtVPNtVTS1qT9sL2kcL2fbnJ50MKW2LJjtXvOgqJk0nKOfnJIlXD=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
